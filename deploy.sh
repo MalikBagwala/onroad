5088edf52c71398b6c85b1d006a33217fa59102d
Malik Bagwala <m.bagwala@gmail.com>
2024-01-29 13:21:58 +0530

    feat: update caddyfiles

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,15 @@
 
 set -e
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+if [ "$RUN_MIGRATIONS" != "0" ]; then
+    python manage.py migrate
+fi
+
 python manage.py createadmin
-gunicorn onroad.wsgi:application --bind 0.0.0.0:9000 --workers=3
+
+if [ "$ENVIRONMENT" = "production" ]; then
+    gunicorn onroad.wsgi:application --bind 0.0.0.0:9000 --workers=3
+else
+    python manage.py runserver 0.0.0.0:9000
+fi
