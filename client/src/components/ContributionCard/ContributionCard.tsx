7a49a3f90513dffbff4b2bea04eb3b38ffd84fb4
Malik Bagwala <m.bagwala@gmail.com>
2024-02-02 21:59:42 +0530

    chore: optimized favicons

--- client/src/components/ContributionCard/ContributionCard.tsx
+++ client/src/components/ContributionCard/ContributionCard.tsx
@@ -5,20 +5,21 @@ import { ActionIcon, Badge, Button, Card, Group, Image, Text } from '@mantine/co
 import { IconLink } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 import classes from './ContributionCard.module.css';
-
-const images = [
-  'https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-  'https://images.unsplash.com/photo-1567767292278-a4f21aa2d36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-  'https://images.unsplash.com/photo-1605774337664-7a846e9cdf17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-  'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-  'https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-];
+import placeholder from '@/images/vehicle_placeholder.webp';
 
 type ContributionCardType = {
   variant?: VariantsListQuery['variants'][0];
 };
 function ContributionCard({ variant }: ContributionCardType) {
   const total = variant?.contributions_aggregate?.aggregate?.avg?.total || 0;
+  const images = [];
+  for (const color of variant?.colors || []) {
+    images.push(...color.attachments.map(({ attachment }) => attachment.url));
+  }
+  if (images.length === 0) {
+    images.push(placeholder);
+  }
+
   const slides = images.map((image) => (
     <Carousel.Slide key={image}>
       <Image src={image} height={220} />
