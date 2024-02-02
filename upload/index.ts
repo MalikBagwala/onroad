c8b68442c7cb78da577ba96beec30e507f6b81a5
Malik Bagwala <m.bagwala@gmail.com>
2024-02-02 09:46:18 +0530

    feat: add basic file upload feature

--- upload/index.ts
+++ upload/index.ts
@@ -1,5 +1,13 @@
 import type { BunFile } from "bun";
 import { nanoid } from "nanoid";
+import { join } from "path";
+import fs from "fs";
+
+try {
+  fs.mkdirSync(join(import.meta.dir, "media"), { recursive: true });
+} catch (error) {
+  console.log(error);
+}
 
 const server = Bun.serve({
   port: 3000,
@@ -9,7 +17,7 @@ const server = Bun.serve({
     // return index.html for root path
 
     // parse formdata at /action
-    if (url.pathname === "/upload" && req.method === "POST") {
+    if (url.pathname === "/uploader/upload" && req.method === "POST") {
       const formdata = await req.formData();
       const files = formdata.getAll("file");
       const saveFilePromises = files.map(async (targetFile, index) => {
@@ -17,7 +25,7 @@ const server = Bun.serve({
         const buffer = await file.arrayBuffer();
         const extension = file.name?.split(".").pop();
         const newFilename = `${nanoid(10)}.${extension}`;
-        const path = `media/${newFilename}`;
+        const path = join(import.meta.dir, "media", newFilename);
         Bun.write(path, buffer);
         return {
           originalName: file.name,
