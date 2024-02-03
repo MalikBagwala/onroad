407586acb658b5938c5b737284ab443d66ed2678
Malik Bagwala <m.bagwala@gmail.com>
2024-02-03 11:55:45 +0530

    Sync submodule 'backups' with latest changes

--- backup_pg.sh
+++ backup_pg.sh
@@ -17,4 +17,26 @@ TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 # Use docker exec to run pg_dumpall inside the PostgreSQL container, and compress the backup file with xz
 docker exec -t ${CONTAINER_NAME} pg_dumpall -U ${POSTGRES_USER} -w | xz -9e > ./backups/db_backup_${TIMESTAMP}.sql.xz
 
-echo "Backup completed at $(date)"
+# Change directory to /backups
+cd ./backups && git pull
+
+# Remove older .sql.xz files
+ls -t ./*.sql.xz | tail -n +4 | xargs rm -f
+
+# Add the backup file to git
+git add .
+
+# Create a git commit with a message containing the backup file name
+git commit -m "chore: backed up db_backup_${TIMESTAMP}.sql.xz"
+
+# Push the backup to the remote repository
+git push
+
+cd ..
+git submodule update --remote --recursive
+git add .
+
+# Commit and push the removal of older files
+git commit -m "Sync submodule 'backups' with latest changes" && git push
+
+echo "Backup completed at $(date), only the last three backup files are retained in the repository."
