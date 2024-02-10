8203109de5527134c96ec5c853cbaa5e09b553df
Malik Bagwala <m.bagwala@gmail.com>
2024-02-10 10:25:20 +0530

    chore: update deploy script

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -4,4 +4,4 @@
 git pull
 
 # Deploy
-docker compose up --build -d --remove-orphans && wait 5 && docker compose restart hasura
+docker compose up --build -d --remove-orphans && sleep 5 && docker compose restart hasura
