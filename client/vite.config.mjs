c5b955ce0c8634cf785682004e85c5818d27706c
Malik Bagwala <m.bagwala@gmail.com>
2024-03-03 08:59:31 +0530

    feat: integrate client with source code

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,16 +1,30 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // import { compression } from 'vite-plugin-compression2';
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: 'malikbagwala',
+      project: 'onroad-client',
+    }),
+  ],
+
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
+
   server: {
     host: '0.0.0.0',
     port: 5000,
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
